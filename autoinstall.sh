25f16c4249a83458fb163d6e42e956cd06f98f84
Aranhb06 <149063895+Aranhb06@users.noreply.github.com>
2025-06-05 11:17:52 +0200

    Update autoinstall.sh

--- autoinstall.sh
+++ autoinstall.sh
@@ -2,27 +2,20 @@
 installvim() {
         #Actualiza el sistema
         sudo apt update
-
         # Instala curl si no lo tienes
         sudo apt install curl -y 
-
         # Instalacion de vim   
-        sudo apt install vim $$ echo "OK"
-
+        sudo apt install vim
         #Descarga y descomprime el archivo con las configuraciones
         curl -o mi-vim-config.tar.gz https://github.com/Aranhb06/Mi-Config-Vim/raw/main/mi-vim-config.tar.gz
-        tar -xzvf mi-vim-config.tar.gz -C ~ && echo "OK"
-
+        tar -xzvf mi-vim-config.tar.gz -C ~ 
         # Instalacion de vimplug
         curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-
         # Instalacion de node.js para coc.plug 
         curl -fsSL https://deb.nodesource.com/setup_18.x | sudo -E bash -
-        sudo apt install -y nodejs && echo "OK"
-
+        sudo apt install -y nodejs 
         #Inicia la instalacion de los plugins con vimplug
         vim +PlugInstall +qall
-
         #Elimina los archivos
         rm -rf mi-vim-config.tar.gz
 }
